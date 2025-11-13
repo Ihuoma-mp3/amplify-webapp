a2eeeb659556670746b3af0ce6b1d29b360f7633
Precious Ihedoro <PreciouIhedoro@my.unt.edu>
2025-11-13 15:23:41 -0600

    Updated homepage content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, Precious! 🎉</h1>
+      <p>Welcome to your first deployed React website hosted on AWS Amplify!</p>
+
+      <p>Edit this page anytime through VS Code and push updates to GitHub —
+      Amplify will automatically redeploy your site.</p>
     </div>
   );
 }
